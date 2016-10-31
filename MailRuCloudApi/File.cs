be777ba0a77f193267f1a79715d11b7e7e3f97bf
rmyakimenko <rmyakimenko@1cbit.ru>
2016-10-31 17:30:49 +0300

    fake dates (must be less than now cause of windows webclient)

--- MailRuCloudApi/File.cs
+++ MailRuCloudApi/File.cs
@@ -112,8 +112,29 @@ namespace MailRuCloudApi
         /// <value>File size.</value>
         internal FileSize PrimarySize { get; set; }
 
-        public DateTime CreationTimeUtc { get; set; }
-        public DateTime LastWriteTimeUtc { get; set; }
-        public DateTime LastAccessTimeUtc { get; set; }
+        public DateTime CreationTimeUtc
+        {
+            get
+            {
+                return DateTime.Now.AddDays(-1);
+            }
+            set { }
+        }
+        public DateTime LastWriteTimeUtc
+        {
+            get
+            {
+                return DateTime.Now.AddDays(-1);
+            }
+            set { }
+        }
+        public DateTime LastAccessTimeUtc
+        {
+            get
+            {
+                return DateTime.Now.AddDays(-1);
+            }
+            set { }
+        }
     }
 }
